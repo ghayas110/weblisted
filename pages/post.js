3e223c4cb39932e4f13840dffe657d31fb6e78c0
admin-logochemist <team@logochemist.com>
2022-05-11 21:07:01 +0500

    rehman

---
+++ pages/post.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from '../styles/post.module.css'
+
+export default function post() {
+    return (
+        <div className='container-fluid' id={styles.header}>
+            <div className='row' id={styles.row_header}>
+                
+               
+            
+               
+
+
+            </div>
+
+        </div>
+    )
+}
