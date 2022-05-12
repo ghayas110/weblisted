df55776e74290907b9b0765545b373f28979413b
admin-logochemist <team@logochemist.com>
2022-05-13 00:46:58 +0500

    rehman

--- pages/Header.js
+++ pages/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from '../../styles/Header.module.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import styles from '../styles/Header.module.css'
 
 export default function Header() {
     return (
