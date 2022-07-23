bf3f58f855995b91a7594d3f0caf776e58a05ee2
admin-logochemist <team@logochemist.com>
2022-07-24 00:56:18 +0500

    rehman

---
+++ components/component/DashboardProductAddForm/DashboardProductAddForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashboardProductAddForm() {
+  return (
+    <div>DashboardProductAddForm</div>
+  )
+}
+
+export default DashboardProductAddForm
--- components/component/SideBarTwo.js
+++ components/component/SideBarTwo.js
@@ -1,4 +1,4 @@
-import styles from '../../../styles/sidebar.module.css'
+import styles from '../../styles/SideBarTwo.module.css'
 import React, { useState, useEffect } from 'react'
 import {
     LineStyle,
@@ -16,7 +16,7 @@ import {
     Menu
 } from '@material-ui/icons'
 
-function Sidebar() {
+function SideBarTwo() {
 
 
 
@@ -37,73 +37,60 @@ function Sidebar() {
                 <div    >
                     <div className={styles.sidebarWrapper}>
                         <div className={styles.sidebarMenu}>
-                            <h3 className={styles.sidebarTitle}>Dashboard</h3>
                             <ul className={styles.sidebarList}>
                                 <span className={`${styles.sidebarListitem} ${styles.active}`}  >
                                     <LineStyle className={styles.sidebarIcon} />
-                                    Home
+                                
                                 </span>
-                                <span  className={styles.sidebarListitem}>
+                                <span className={styles.sidebarListitem}>
                                     <Timeline className={styles.sidebarIcon} />
-                                    Analytics
                                 </span>
-                                <span  className={styles.sidebarListitem}>
+                                <span className={styles.sidebarListitem}>
                                     <TrendingUp className={styles.sidebarIcon} />
-                                    Sales
                                 </span>
                             </ul>
                         </div>
                         <div className={styles.sidebarMenu}>
-                            <h3 className={styles.sidebarTitle}>Quick Menu</h3>
                             <ul className={styles.sidebarList}>
                                 <span className={`${styles.sidebarListitem}`}>
                                     <PermIdentity className={styles.sidebarIcon} />
-                                    <a href="/UserList">Users</a>
                                 </span>
                                 <span className={styles.sidebarListitem}>
                                     <Storefront className={styles.sidebarIcon} />
-                                    Products
                                 </span>
                                 <span className={styles.sidebarListitem}>
                                     <AttachMoney className={styles.sidebarIcon} />
-                                    Transaction
                                 </span>
                                 <span className={styles.sidebarListitem}>
                                     <BarChart className={styles.sidebarIcon} />
-                                    Reports
                                 </span>
                             </ul>
                         </div>
                         <div className={styles.sidebarMenu}>
-                            <h3 className={styles.sidebarTitle}>Notifications</h3>
                             <ul className={styles.sidebarList}>
-                                <span className={`${styles.sidebarListitem}`}>
+                                <span className={`${styles.sidebarListitem} `}>
                                     <MailOutline className={styles.sidebarIcon} />
-                                    Mail
+                                    
                                 </span>
                                 <span className={styles.sidebarListitem}>
                                     <DynamicFeed className={styles.sidebarIcon} />
-                                    Feedback
+                                    
                                 </span>
                                 <span className={styles.sidebarListitem}>
                                     <ChatBubbleOutline className={styles.sidebarIcon} />
-                                    Messages                    </span>
+                                     </span>
                             </ul>
                         </div>
                         <div className={styles.sidebarMenu}>
-                           <h3 className={styles.sidebarTitle}>Staff</h3>
                             <ul className={styles.sidebarList}>
                                 <span className={`${styles.sidebarListitem}`}>
                                     <WorkOutline className={styles.sidebarIcon} />
-                                    Manage
                                 </span>
                                 <span className={styles.sidebarListitem}>
                                     <Timeline className={styles.sidebarIcon} />
-                                    Analytics
                                 </span>
                                 <span className={styles.sidebarListitem}>
                                     <Report className={styles.sidebarIcon} />
-                                    Reports
                                 </span>
                             </ul>
                         </div>
@@ -115,4 +102,4 @@ function Sidebar() {
     )
 }
 
-export default Sidebar
+export default SideBarTwo;
